e274f767d53eeaad8db9ebfaf8821aa25224a8d6
Sharif <developersharif@yahoo.com>
2021-09-14 12:50:20 +0600

    waiting value changed

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ hrs = askinteger("Input", "Hours")
 mint = askinteger("Input", "Minutes")
 wait =  (hrs * 3600) + (mint * 60) 
 while True:
-     time.sleep(3)
+     time.sleep(wait)
      print("trigared")
      gui()

5da2af1b9edb18201c16d2f33e237a57343b5d9e
Sharif <developersharif@yahoo.com>
2021-09-14 17:29:39 +0600

    gui improved with few functionalities

--- lib/src.py
+++ lib/src.py
@@ -3,35 +3,37 @@ from tkinter.simpledialog import askinteger
 from tkinter import *
 from PIL import ImageTk,Image 
 import time
-root = tk.Tk()
 class gui():
      def __init__(self):
-        global root; 
-        root.overrideredirect(True)
-        root.overrideredirect(True)
-        root.title("Break Timer")
-        root.attributes('-fullscreen',True)
-        root.configure(bg='white')
-        Label = tk.Label(root, 
-                          text = 'You Have To Break Some Time',bg="white",fg="Black",font=("Courier", 40)).pack(pady=5) 
-        Label = tk.Label(root, 
-                          text = "Once you get to that point when you can just be yourself and relax, I just think that you're so much happier in general.",bg="white",fg="Black",font=("Courier", 10)).pack(pady=10) 
+        self.root = Tk()
+        self.root.title("Break Timer")
+        ico = Image.open('lib/assets/timer.ico')
+        photo = ImageTk.PhotoImage(ico)
+        self.root.wm_iconphoto(False, photo)
+        self.root.overrideredirect(True)
+        self.root.overrideredirect(False)
+        self.root.title("Break Timer")
+        self.root.attributes('-fullscreen',True)
+        self.root.configure(bg='#5C5C5C')
+        Label = tk.Label(self.root, 
+                          text = 'Rest your eyes. Stretch your legs. Breathe. Relax.',bg="#5C5C5C",fg="white",font=("Helvetica", 40)).pack(pady=5) 
+        Label = tk.Label(self.root, 
+                          text = "Time for a break!",bg="#5C5C5C",fg="white",font=("Courier", 10)).pack(pady=10) 
         
-        button = tk.Button(root, 
+        button = tk.Button(self.root, 
                   text = 'Quit', bg="skyblue",fg="white",
                   command=quit).pack(pady=8)
-        bg_canv = Canvas(root,width=500,height=600,bg="white",border=0)
+        bg_canv = Canvas(self.root,width=500,height=600,bg="#5C5C5C",borderwidth=0,highlightthickness = 0)
         image  = ImageTk.PhotoImage(Image.open("lib/assets/img/no-bg-timer.png"))
         bg_canv.create_image(0,0,anchor=NW,image=image)
         bg_canv.pack()
-        root.mainloop()
+        self.root.protocol("WM_DELETE_WINDOW", quit)
+        self.root.mainloop()
         
-     def getUptime():
-        startTime = time.time()
-        print(time.time() - startTime)
-
      def quit():
-        root.destroy()
+         if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.root.destroy()
 
 
-     
+def job():
+   gui()     
--- run.py
+++ run.py
@@ -1,9 +1,51 @@
 from lib.src import *
+import schedule
+import os
+import time
+#default value...
+Hours = 2
+Minutes = 10
+
+#close windwo after setup the ask_time()
+def close():
+    root.destroy() 
+    
+#custom break time 
+def ask_time():
+     global Hours,Minutes
+     Hours = askinteger("Input", "Break frequency: Set Hours")
+     Minutes = askinteger("Input", "Break frequency: Set Minutes")
+     tk.Button(root, 
+                  text = 'Exit', bg="red",fg="white",
+                  command=close).pack(pady=8)
+     
+ 
+root = tk.Tk()
 root.title("Break Timer")
-hrs = askinteger("Input", "Hours")
-mint = askinteger("Input", "Minutes")
-wait =  (hrs * 3600) + (mint * 60) 
+root.geometry("500x500")
+     
+ico = Image.open('lib/assets/timer.ico')
+photo = ImageTk.PhotoImage(ico)
+root.wm_iconphoto(False, photo)
+tk.Label(root,text="Default Break frequency {} Hours {} Minutes".format(Hours,Minutes)).pack(pady=9)
+tk.Button(root, 
+                  text = 'Custom time', bg="black",fg="white",
+                  command=ask_time).pack(pady=8)
+root.iconify()
+root.mainloop()
+
+
+wait =  (Hours * 3600) + (Minutes * 60) 
+
+def job():
+    os.system("python3 main.py")
+ 
+   
+print(wait)
+schedule.every(wait).seconds.do(job)
+
 while True:
-     time.sleep(wait)
-     print("trigared")
-     gui()
+    schedule.run_pending()
+    time.sleep(1)
+    
+    
